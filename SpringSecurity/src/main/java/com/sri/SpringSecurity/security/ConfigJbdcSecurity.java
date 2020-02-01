38ff00fd89e8166691d55336319e53660d224d3f
sriramede9 <only4programming03@gmail.com>
2020-02-01 10:34:02 -0500

    How to support security using JPA and H2 DB

---
+++ SpringSecurity/src/main/java/com/sri/SpringSecurity/security/ConfigJbdcSecurity.java
@@ -0,0 +1,55 @@
+package com.sri.SpringSecurity.security;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@Configuration
+public class ConfigJbdcSecurity extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	DataSource dataSource;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		// TODO Auto-generated method stub
+		
+		
+		
+		auth
+		.jdbcAuthentication()
+		.dataSource(dataSource);
+	
+		
+		
+	}
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+	
+		http.
+		authorizeRequests()
+		.antMatchers("/hi","/admin").hasRole("ADMIN") //most restrictive
+		.antMatchers("/hi","/user").hasRole("USER") //less restrictive
+		.antMatchers("/h2-console").permitAll()
+		.and()
+		.formLogin();
+	}
+
+	
+}
--- SpringSecurity/src/main/java/com/sri/SpringSecurity/security/ConfigSecurity.java
+++ SpringSecurity/src/main/java/com/sri/SpringSecurity/security/ConfigSecurity.java
@@ -1,68 +1,68 @@
-package com.sri.SpringSecurity.security;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-@EnableWebSecurity
-@Configuration
-public class ConfigSecurity extends WebSecurityConfigurerAdapter {
-
-//	@Override
-//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//		auth.inMemoryAuthentication().withUser("Sri").password("sri").roles("USER").and().withUser("Sri ram").password("sri").roles("ADMIN");
+//package com.sri.SpringSecurity.security;
 //
-//	}
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.User.UserBuilder;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 //
+////@EnableWebSecurity
+////@Configuration
+//public class ConfigSecurity extends WebSecurityConfigurerAdapter {
+//
+////	@Override
+////	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+////		auth.inMemoryAuthentication().withUser("Sri").password("sri").roles("USER").and().withUser("Sri ram").password("sri").roles("ADMIN");
+////
+////	}
+////
+////	@Bean
+////	public PasswordEncoder getPasswordEncoder() {
+////		return NoOpPasswordEncoder.getInstance();
+////	}
+//	
+//	
+//	//second approach as per documentation
+//	
 //	@Bean
-//	public PasswordEncoder getPasswordEncoder() {
-//		return NoOpPasswordEncoder.getInstance();
+//	public UserDetailsService userDetailsService()  {
+//	    // ensure the passwords are encoded properly
+//	    UserBuilder users = User.withDefaultPasswordEncoder();
+//	    InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//	    manager.createUser(users.username("user").password("password").roles("USER").build());
+//	    manager.createUser(users.username("admin").password("password").roles("USER","ADMIN").build());
+//	    return manager;
 //	}
-	
-	
-	//second approach as per documentation
-	
-	@Bean
-	public UserDetailsService userDetailsService()  {
-	    // ensure the passwords are encoded properly
-	    UserBuilder users = User.withDefaultPasswordEncoder();
-	    InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-	    manager.createUser(users.username("user").password("password").roles("USER").build());
-	    manager.createUser(users.username("admin").password("password").roles("USER","ADMIN").build());
-	    return manager;
-	}
-
-	
-	
-	//now use HttpSecurity to filter the requests based on roles
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-
+//
+//	
+//	
+//	//now use HttpSecurity to filter the requests based on roles
+//	
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//
+////		http.
+////		authorizeRequests()
+////		.antMatchers("/**")  //all incoming requests
+////		.hasAnyRole("ADMIN") //only admin can request
+////		.and()
+////		.formLogin();        //and should login using form
+//		
 //		http.
 //		authorizeRequests()
-//		.antMatchers("/**")  //all incoming requests
-//		.hasAnyRole("ADMIN") //only admin can request
+//		.antMatchers("/hi","/admin").hasRole("ADMIN") //most restrictive
+//		.antMatchers("/hi","/user").hasRole("USER") //less restrictive
+//		.antMatchers("/").permitAll()
 //		.and()
-//		.formLogin();        //and should login using form
-		
-		http.
-		authorizeRequests()
-		.antMatchers("/hi","/admin").hasRole("ADMIN") //most restrictive
-		.antMatchers("/hi","/user").hasRole("USER") //less restrictive
-		.antMatchers("/").permitAll()
-		.and()
-		.formLogin();
-	}
-	
-
-}
+//		.formLogin();
+//	}
+//	
+//
+//}
