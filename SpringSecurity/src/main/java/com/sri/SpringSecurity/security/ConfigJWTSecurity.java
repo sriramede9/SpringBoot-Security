df31594291913731593d54d89294bf73b03f3911
sriramede9 <only4programming03@gmail.com>
2020-02-03 20:55:59 -0500

    Fixed bug send post req to /autenticate will get jwt token

--- SpringSecurity/src/main/java/com/sri/SpringSecurity/security/ConfigJWTSecurity.java
+++ SpringSecurity/src/main/java/com/sri/SpringSecurity/security/ConfigJWTSecurity.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,6 +14,7 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.sri.SpringSecurity.service.MyUserDetailsServiceJWT;
 
@@ -36,5 +38,22 @@ public class ConfigJWTSecurity extends WebSecurityConfigurerAdapter {
 	@Bean
 	public AuthenticationManager authenticationManagerBean() throws Exception {
 		return super.authenticationManagerBean();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+// We don't need CSRF for this example
+		httpSecurity.csrf().disable()
+// dont authenticate this particular request
+				.authorizeRequests().antMatchers("/authenticate").permitAll().
+// all other requests need to be authenticated
+				anyRequest().authenticated();
+		// .and().
+// make sure we use stateless session; session won't be used to
+// store user's state.
+//				exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
+//				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+// Add a filter to validate the tokens with every request
+//		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 	}
 }
